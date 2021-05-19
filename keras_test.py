739519db6b007ab1ce5e1b19e9d37588c3c9139d
Satoshi Ebato <Satoshi.Ebato@sony.com>
2021-05-19 15:40:15 +0900

    keras_test import宣言対応

---
+++ keras_test.py
@@ -0,0 +1,16 @@
+#!python3
+# -*- coding: utf-8
+
+import keras
+from keras.utils import np_utils
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+print("\n Success.")
