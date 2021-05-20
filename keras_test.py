b17f2b4005e6ba184c339dfc6a34e5e264b992b1
サトシ えば <cero0x5345@hotmail.co.jp>
2021-05-20 22:16:58 +0900

    画像拡張子を2つまで対応

--- keras_test.py
+++ keras_test.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 def list_pictures(directory, ext='jpg|png'):
     return [os.path.join(root, f)
             for root, _, files in os.walk(directory) for f in files
-            if re.match(r'([\w]+\.(?:' + ext + '))', f.lower())]
+            if re.match(r'([\w]+\.([\w]*\.)*(?:' + ext + '))', f.lower())]
 
 print("\n Pre load Success.")
 
