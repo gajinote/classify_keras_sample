1652db9907185f657144016fa27daa1086ab78ad
Gaji Note <gaji_note@seigaji.info>
2021-11-10 16:17:52 +0900

    パラメータ削減

--- keras_learn.py
+++ keras_learn.py
@@ -128,24 +128,17 @@ model = Sequential()
 
 model.add(Conv2D(64, (3, 3), padding='same',
                  input_shape=X_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.4))
-
-model.add(Conv2D(64, (3, 3), padding='same'))
-model.add(Activation('relu'))
 model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Activation('relu'))
 model.add(Dropout(0.4))
 
 model.add(Flatten())
 model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(512))
+model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(2))       # クラスは2個
